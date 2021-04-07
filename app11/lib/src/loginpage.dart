686a3316860c4f5a2e7155e46875711528fb94bc
GustavoSMelo <gsantos15569@gmail.com>
2021-04-06 21:55:37 -0300

    learning about Card form

--- app11/lib/src/loginpage.dart
+++ app11/lib/src/loginpage.dart
@@ -13,12 +13,37 @@ class LoginPage extends StatelessWidget {
             Text('Login Form', textDirection: TextDirection.ltr, style: TextStyle(
               fontSize: 32,
             ),),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-                fillColor: Colors.indigo
-              )
+            Card(
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      fillColor: Colors.indigo
+                    )
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'password',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login', textDirection: TextDirection.ltr),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurple[400],
+                onPrimary: Colors.deepPurple[300],
+                textStyle: TextStyle(
+                  color: Colors.white
+                )
+              ),
             )
           ],
         )
@@ -32,6 +57,29 @@ class LoginPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Stack', textDirection: TextDirection.ltr),
           backgroundColor: Colors.deepPurple[300],
+        ),
+        drawer: Drawer(
+          child: (
+            Column(
+              children: [
+                UserAccountsDrawerHeader(
+                  currentAccountPicture: ClipRRect(
+                    child: Image.network('https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_960_720.png'),
+                    borderRadius: BorderRadius.circular(40), 
+                  ),
+                  accountName: Text('Gustavo Santos Melo'), 
+                  accountEmail: Text('gsantos15569@gmail.com'),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[200]
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text('Home '),
+                )
+              ],
+            )
+          ),
         ),
         body: Stack(children: [
           Container(
