df54d460f4b3c1e98839733e0c5d7073af55ff70
GustavoSMelo <gsantos15569@gmail.com>
2021-04-12 02:57:43 -0300

    controller done

--- currency_converter/lib/src/controller/homepageController.dart
+++ currency_converter/lib/src/controller/homepageController.dart
@@ -1,3 +1,32 @@
 class HomePageController {
   
+  convert (String currentCurrency, String converterCurrency, double currentCurrencyPrice) {
+
+    if (currentCurrency == 'Real') {
+
+      if (converterCurrency == 'Dolar') {
+        return currentCurrencyPrice * 5.7;
+      } else if (converterCurrency == 'Euro') {
+        return currentCurrencyPrice * 6.8;
+      }
+
+    } else if (currentCurrency == 'Dolar') {
+
+      if (converterCurrency == 'Real') {
+        return currentCurrencyPrice * 5.7;
+      } else if (converterCurrency == 'Euro') {
+        return currentCurrencyPrice * 0.84;
+      }
+
+    } else {
+      if (converterCurrency == 'Real') {
+        return currentCurrencyPrice * 5.8;
+      } else if (converterCurrency == 'Dolar') {
+        return currentCurrencyPrice * 1.16;
+      }
+    }
+
+    return currentCurrencyPrice;
+  }
+
 }
