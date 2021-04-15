61ddb5ff9b4b512d24435360d424c3a3144002ae
GustavoSMelo <gsantos15569@gmail.com>
2021-04-14 23:59:54 -0300

    main file resolved

--- app12/lib/main.dart
+++ app12/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app12/src/homepage.dart';
+import 'package:app12/src/views/homepage.dart';
 import 'package:flutter/material.dart';
 
 main () => runApp(MaterialApp(home: HomePage()));
