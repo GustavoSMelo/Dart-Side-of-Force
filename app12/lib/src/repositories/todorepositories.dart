3d3d40c227722aaa89721c39c5439615851e9232
GustavoSMelo <gsantos15569@gmail.com>
2021-04-14 23:59:15 -0300

    the code to get informations in api

--- app12/lib/src/repositories/todorepositories.dart
+++ app12/lib/src/repositories/todorepositories.dart
@@ -6,18 +6,19 @@ class TodoRepository {
   Dio dio = Dio();
   String url = 'https://jsonplaceholder.typicode.com/todos';
 
-  Future<List<TodoModel>> fetch () async  {
+  Future<List<TodoModel>> fetch () async {
     var response = await dio.get(url);
     var list = response.data as List;
 
-    List<TodoModel> listTodos = [];
+    return list.map((item) => TodoModel.fromJson(item)).toList();
+
+    /*List<TodoModel> listTodos = [];
 
     for (var item in list) {
       var todo = TodoModel.fromJson(item); 
       listTodos.add(todo);
     }
 
-    return listTodos;
+    return listTodos;*/
   }
-
 }
